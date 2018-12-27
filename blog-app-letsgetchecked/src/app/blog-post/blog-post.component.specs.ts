cd72ee4f911711984095d49955bee1e12118e70d
PreeyaK <priya.khopde11@gmail.com>
2018-12-27 15:03:26 +0000

    Update blog-post.component.specs.ts

--- blog-app-letsgetchecked/src/app/blog-post/blog-post.component.specs.ts
+++ blog-app-letsgetchecked/src/app/blog-post/blog-post.component.specs.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {} from 'jasmine';
 import { BlogPostComponent } from './blog-post.component';
 
 describe('BlogPostComponent', () => {
